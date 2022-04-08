90100a1bbedc96dc7295223dd6105b88c904ef34
ravi0027 <ravi@bootlabstech.com>
2022-04-08 17:17:12 +0530

    remove security

--- eleastic cache/main.tf
+++ eleastic cache/main.tf
@@ -9,25 +9,25 @@ resource "aws_vpc" "aws-vpc" {
 
 
 
-resource "aws_security_group" "aws-security" {
-  name = var.security_group_name
-  description = "Allow ssh from office"
-  vpc_id = aws_vpc.aws-vpc.id
+# resource "aws_security_group" "aws-security" {
+#   name = var.security_group_name
+#   description = "Allow ssh from office"
+#   vpc_id = aws_vpc.aws-vpc.id
   
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port = 80
+#     to_port = 80
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port = 0
+#     to_port = 0
+#     protocol = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 resource "aws_elasticache_security_group" "aws-security" {
   name                 = var.elasticache_security_group_name
