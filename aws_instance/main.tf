4e435f3b0b8577427f9b1d0c587d50f6a087e5fa
sreemugesh <101329338+smuges@users.noreply.github.com>
2022-04-20 15:24:58 +0530

    changes

--- aws_instance/main.tf
+++ aws_instance/main.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-    region = var.region
-    access_key = var.access_key
-    secret_key = var.secret_key
-}
+
 
 resource "aws_vpc" "first_vpc" {
   cidr_block       = "10.0.0.0/16"
