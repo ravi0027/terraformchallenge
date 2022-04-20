a1b6a7a372dc39ea3fcc84bccceaedf780340f3e
sreemugesh <101329338+smuges@users.noreply.github.com>
2022-04-20 15:48:29 +0530

    azure app services

---
+++ azure_app/main.tf
@@ -0,0 +1,36 @@
+
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup-${random_integer.ri.result}"
+  location = var.location
+}
+# Create the Linux App Service Plan
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = "webapp-asp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = var.skutier
+    size = var.skusize
+  }
+}
+# Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub/Bitbucket repo
+resource "azurerm_app_service" "webapp" {
+  name                = "webapp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+
+  source_control {
+    repo_url           = var.repourl
+    branch             = var.branch
+    manual_integration = true
+    use_mercurial      = false
+  }
+
+}
