8d06136a9a56653cce5abd71a6104426792ef6c9
praveenj-bl <praveen.jagannath@bootlabstech.com>
2022-04-20 16:06:22 +0530

    Updated

--- Azure_LB/main.tf
+++ Azure_LB/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.99.0"
-    }
-  }
-
-  required_version = "1.1.2"
-}
 
 provider "azurerm" {
   features {}
--- Azure_MySql_SS/main.tf
+++ Azure_MySql_SS/main.tf
@@ -1,13 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.99.0"
-    }
-  }
 
-  required_version = "1.1.2"
-}
 
 provider "azurerm" {
   features {}
